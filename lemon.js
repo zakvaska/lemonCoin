5dd3ec5def975b0ea20bf8b57368883e1664fd35
Anton Kupchenko <anton_kupchenko@epam.com>
2021-09-14 22:46:24 +0300

    added profit accrual

--- lemon.js
+++ lemon.js
@@ -1,48 +1,48 @@
-// class Stage {
-//     constructor (multiplier, tokenCount, transToRise, price, stageIndex) {
-//     this.properties = {        
-//         multiplier: multiplier,
-//         tokenCount: tokenCount,
-//         transToRise: transToRise,
-//         price: price,
-//         stageIndex: stageIndex
-//     }
-//   }
-// }
-
-// let stages = [];
-
-
-// stages.push(new Stage(multiplier, 0, 0, 0.01, 0));
-// stages.push(new Stage(multiplier, 55, 0, 0.02, 1));
-// stages.push(new Stage(multiplier, 55, 0, 0.04, 2));
-// stages.push(new Stage(multiplier, 55, 150, 0.05, 3));
-// stages.push(new Stage(multiplier, 55, 150, 0.06, 4));
-// stages.push(new Stage(multiplier, 55, 150, 0.07, 5));
-// stages.push(new Stage(multiplier, 55, 150, 0.08, 6));
-// stages.push(new Stage(multiplier, 55, 150, 0.08, 7));
-
-// console.log(stages);
-
-// let currentStage = stages[0];
-// let currentStageIndex = currentStage.properties.stageIndex;
-
 console.log('hello')
 
 
 const step = 0.001;
 let tokenPrice = 0.01;
 const split = 100;
-const tokenCount = 80000000;
+const totalTokenCount = 80000000;
 // let totalPackagesSold = 0;
-let totalPackCoef = 0;
+let globalPackCoef = 0;
+let globalTransCount = 0;
+let globalTokensSold = 0;
+
+let totalPackPurchases = 0;
+let totalTokensSold = 0;
+
+let totalTokenPaidProfit = 0;
+
+let totalTokensRemain = totalTokenCount;
  
 
+class Stage {
+    constructor (transCount,tokensSold, price) {
+    this.properties = {  
+        transCount: transCount,      
+        tokensSold: tokensSold,        
+        price: price
+    }
+    totalPackPurchases += transCount;
+    totalTokensSold += tokensSold;
+    tokenPrice += step;
+    globalPackCoef = 0;
+    globalTransCount = 0;
+    globalTokensSold = 0;
+  }
+}
+
+let stages = [];
+
 class Package {
-    constructor(price, iterationCoef) {
+    constructor(price, iterationCoef, profitRate) {
         this.properties = {
             price: price,
-            iterationCoef: iterationCoef
+            iterationCoef: iterationCoef,
+            profitRate: profitRate,
+            isActive: true
         }
     }
 }
@@ -66,8 +66,8 @@ for (let i = 0; i < 8; i++) {
             break;
     }
 
-    packages.push(new Package(10 * Math.pow(2, i), coef));
-    console.log(packages);
+    packages.push(new Package(10 * Math.pow(2, i), coef, 0.1 + 0.014 * i));
+    // console.log(packages);
 
 }
 
@@ -87,16 +87,22 @@ class User {
             purchaseAmount: 0,
             tokenCount: 0,
             refferalCount: 0,
-            parentRef: parent
+            parentRef: parent,
+            packages: []
         }
     }
 
-    buyPackage(packageId) {
-        this.properties.purchaseCount++;
-        const currentPackage = packages[packageId];
-        this.properties.purchaseAmount += currentPackage.properties.price;
-        this.properties.tokenCount += currentPackage.properties.price / tokenPrice;
-        totalPackCoef += currentPackage.properties.iterationCoef;
+    buyPackage(currentPack) {        
+        this.properties.purchaseCount++;        
+        this.properties.packages.push(currentPack);
+        this.properties.purchaseAmount += currentPack.properties.price;
+        const purchasedTokens = currentPack.properties.price / tokenPrice
+        this.properties.tokenCount += purchasedTokens;
+        totalTokensRemain -= purchasedTokens;
+        globalTransCount++;
+        globalTokensSold += purchasedTokens;
+        globalPackCoef += currentPack.properties.iterationCoef;
+        
         // console.log('purchase!');
         this.checkCoef();
     }
@@ -104,16 +110,17 @@ class User {
     buyRandPackage() {        
         const packageId = getPackageId();
         // console.log(packages[packageId].properties.price);
-        this.buyPackage(packageId);        
+        this.buyPackage(packageId);                
     }
 
     
-
     //with progression
     buyAllPacks() {
-        for (let i = 0; i < packages.length; i++) {
-            this.buyPackage(i);
-        }        
+        packages.forEach((pack) => {
+            if (pack.properties.isActive) {
+                this.buyPackage(pack);
+            }     
+        });              
     }
 
     inviteFriend() {
@@ -123,33 +130,74 @@ class User {
     }
 
     checkCoef() {
-        if (totalPackCoef >= split) {
-            this.increasePrice();
+        if (globalPackCoef >= split) {
+            this.levelUp();
         }
     }
 
-    increasePrice() {
-        tokenPrice += step;
-        totalPackCoef -= split;
+    levelUp() {
+        const deactivatePack = (packageId) => {
+            packages[packageId].properties.isActive = false;
+        }
+
+        stages.push(new Stage(globalTransCount, globalTokensSold, tokenPrice));        
+        if (stages.length <= 3) {
+            // console.log(stages.length - 1);
+            deactivatePack(stages.length - 1);            
+        }
         console.log('new stage!');
     }
 }
 
-for (let i = 0; i < 500; i++) {
-    let user = new User(null);
-    users.push(user);
-//     user.buyRandPackage();
-    user.buyAllPacks();
-//     user.inviteFriend();
-    // console.log(users); 
+
+
+const emulateCycle = (newMainUsersCount) => {
+    const addMainUsers = (newMainUsersCount) => {
+        for (let i = 0; i < newMainUsersCount; i++) {
+            let user = new User(null);
+            users.push(user);
+            
+        }
+    }
+
+    const allUsersBuyAllPacks = () => {
+        users.forEach((user) => {
+            // user.buyRandPackage();
+            user.buyAllPacks();
+            // user.inviteFriend();
+            // console.log(users); 
+        })
+    }
+
+    const accrueProfit = () => {
+        users.forEach((user) => {
+            user.properties.packages.forEach((package) => {
+                let tokenProfit = (package.properties.price * package.properties.profitRate) / tokenPrice;                
+                user.properties.tokenCount += tokenProfit;
+                totalTokenPaidProfit += tokenProfit;
+                totalTokensRemain -= tokenProfit
+                // console.log(profit);
+            })
+            
+        })
+    }
+
+    addMainUsers(newMainUsersCount);
+    allUsersBuyAllPacks();
+    // console.log(users[0].properties.tokenCount);
+    accrueProfit();
+    // console.log(users[0].properties.tokenCount);    
 }
 
+emulateCycle(500);
 
+console.log('globalPackCoef = ' + globalPackCoef);
+console.log('tokenPrice = ' + new Intl.NumberFormat('ru-RU').format(tokenPrice.toFixed(2)));
+console.log('totalTokenCount = ' + new Intl.NumberFormat('ru-RU').format(totalTokenCount.toFixed(2)));
+console.log('totalTokensSold = ' + new Intl.NumberFormat('ru-RU').format(totalTokensSold.toFixed(2)));
+console.log('totalTokenPaidProfit = ' + new Intl.NumberFormat('ru-RU').format(totalTokenPaidProfit.toFixed(2)));
+console.log('totalTokensRemain = ' + new Intl.NumberFormat('ru-RU').format(totalTokensRemain.toFixed(2)));
+console.log('totalPackPurchases = ' + totalPackPurchases);
 
-
-
-// const levelUp = () => {    
-//     price += 
-// }
-console.log('totalPackCoef = ' + totalPackCoef);
-console.log('tokenPrice = ' + tokenPrice);
+console.log(stages);
+console.log(users);
